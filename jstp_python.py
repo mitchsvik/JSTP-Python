af439f3a0ef40542b70350de66af7e59b0890d01
Veniamin Zinevych <mitchsvik@gmail.com>
2016-04-24 20:27:25 +0300

    version 0.1.1
    +associative array (dictionary) parsing function;
    +boolean and undefined types "parsing";

--- jstp_python.py
+++ jstp_python.py
@@ -1,75 +1,128 @@
-#JSTP lib for Python 3.5.1
+# JSTP lib for Python 3.5.1
 
 # Deserialize array of scalar or array of array
 # no objects allowed, just arrays and values
 #   string - array serialized to string
 #   return - deserialized array
+#
+
 def deserialize(string, index = 0):
-    result = []
-    index = __skipp_white_spaces(string, index)
     if string[index] != '[':
-        return result
+        return []
+    result = []
     index += 1
     while True:
-        index = __skipp_white_spaces(string, index)
         if string[index] == '\'':
             res, index = __parse_string(string, index)
-            result.append(res)
         elif str.isdigit(string[index]) or string[index] == '-':
             res, index = __parse_number(string, index)
-            result.append(res)
+        elif string[index] == 't' or string[index] == 'f':
+            res, index = __parse_boolean(string, index)
+        elif string[index] == 'u':
+            res, index = __parse_undefined(string, index)
         elif string[index] == '[':
             res, index = deserialize(string, index)
-            result.append(res)
+        result.append(res)
+
         if string[index] == ']':
             index+=1
+            if index == len(string):
+                return result
             return (result, index)
         else:
             index+=1
 
-def parse():
-    pass
+# Deserialize string to object, just data: objects and arrays
+# no expressions and functions allowed in object definition
+#   str - object serialized to string
+#   return - deserialized object
+#
 
-def interprete():
-    pass
+def parse(string, index = 0):
+    if string[index] == '{':
+        result = {}
+        type = 'dict'
+    elif string[index] == '[':
+        result = []
+        type = 'array'
+    else:
+        return {}
+    key = 0
+    index += 1
+
+    while True:
+        if string[index] == '\'':
+            res, index = __parse_string(string, index)
+        elif str.isdigit(string[index]) or string[index] == '-':
+            res, index = __parse_number(string, index)
+        elif string[index] == 't' or string[index] == 'f':
+            res, index = __parse_boolean(string, index)
+        elif string[index] == '[' or string[index] == '{':
+            res, index = parse(string, index)
+        result, key = __append(result, res, type, key)
+
+        if string[index] == ']' or string[index] == '}':
+            index+=1
+            if index == len(string):
+                return result
+            return (result, index)
+        else:
+            index += 1
+
+# TODO Interprete function
+#def interprete():
 
 def __skipp_white_spaces(string, index):
     while string[index] == ' ' or string[index] == '\t':
         index += 1
+
     return index
 
-def __parse_string(string, index):
-    end_point = ''
-    out = ''
+def __append(result, res, type, key):
+    if type == 'dict':
+        result[key] = res
+        key += 1
+    else:
+        result.append(res)
+    return result, key
 
+def __parse_string(string, index):
     if string[index] == '\'':
         end_point = '\''
-    else :
+    else:
         end_point = '\"'
-    index+= 1
+    index += 1
+    start = index
 
     while string[index] != end_point:
-        out += string[index]
         index += 1
-    index+=1
-    return (out, index)
+    index += 1
+
+    return (string[start:index-1], index)
 
 def __parse_number(string, index):
-    out = ''
-
     is_negative = False
     if string[index] == '-':
         is_negative = True
         index+=1
+    start = index
 
     while str.isdigit(string[index]) or string[index] == '.':
-        out += string[index]
         index += 1
 
     try:
-        res = int(out)
+        res = int(string[start:index])
     except ValueError:
-        res = float(out)
+        res = float(string[start:index])
     if is_negative:
         res = 0 - res
     return (res, index)
+
+def __parse_boolean(string, index):
+    if string[index] == 't':
+        return (True, index+4)
+    else:
+        return (False, index+5)
+
+def __parse_undefined(string, index):
+    return (None, index+9)
--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import jstp_python as api_jstp
 
-test = "['Marcus Aurelius','AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy','37','158']]"
-out = api_jstp.deserialize(test)
-data = out[0]
+test = "['Marcus Aurelius','AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy','37','158','']]"
+data = api_jstp.deserialize(test)
 print(data)
 
 test = "['Marcus Aurelius','AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy',37,158]]"
-out = api_jstp.deserialize(test)
-data = out[0]
+data = api_jstp.deserialize(test)
 print(data)
 
+test = "['Marcus Aurelius','AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy',-37,undefined],false,true]"
+data = api_jstp.deserialize(test)
+print(data)
 
-test = "['Marcus Aurelius','AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy',-37,158]]"
-out = api_jstp.deserialize(test)
-data = out[0]
+test = "{'Marcus Aurelius', 'AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy',-37,158]}"
+data = api_jstp.parse(test)
 print(data)
