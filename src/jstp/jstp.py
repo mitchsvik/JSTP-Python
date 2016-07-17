b8e0652eedd870733e9d829ca77a9548c8ab8885
Veniamin Zinevych <mitchsvik@gmail.com>
2016-07-17 15:29:54 +0300

    Some code refactoring
    + Basic protocol constants;
    + Multiline docstrings;
    * Pep8 code refactor;

--- src/jstp/jstp.py
+++ src/jstp/jstp.py
@@ -1,12 +1,20 @@
-# JSTP lib for Python 3.5.1
+# JSTP (JavaScript Transport Protocol) library for Python 3
 
-# Deserialize array of scalar or array of array
-# no objects allowed, just arrays and values
-#   string - array serialized to string
-#   return - deserialized array
-#
+PACKET_DELIMITER = ',{\f},'
+DELIMITER_LENGTH = len(PACKET_DELIMITER)
+CHUNKS_FIRST = ['[']
+CHUNKS_LAST = [']']
+HANDSHAKE_TIMEOUT = 3000
 
-def deserialize(string, index = 0):
+
+def deserialize(string, index=0):
+    """
+    Deserialize array of scalar or array of array
+    no objects allowed, just arrays and values
+    :param string: array serialized to string
+    :param index: current position in string
+    :return: deserialized array
+    """
     if string[index] != '[':
         return []
     result = []
@@ -26,26 +34,28 @@ def deserialize(string, index = 0):
         result.append(res)
 
         if string[index] == ']':
-            index+=1
+            index += 1
             if index == len(string):
                 return result
-            return (result, index)
+            return result, index
         else:
-            index+=1
+            index += 1
 
-# Deserialize string to object, just data: objects and arrays
-# no expressions and functions allowed in object definition
-#   str - object serialized to string
-#   return - deserialized object
-#
 
-def parse(string, index = 0):
+def parse(string, index=0):
+    """
+    Deserialize string to object, just data: objects and arrays
+    no expressions and functions allowed in object definition
+    :param string: object serialized to string
+    :param index: current position in string
+    :return: deserialized object
+    """
     if string[index] == '{':
         result = {}
-        type = 'dict'
+        type_ = 'dict'
     elif string[index] == '[':
         result = []
-        type = 'array'
+        type_ = 'array'
     else:
         return {}
     key = 0
@@ -60,32 +70,31 @@ def parse(string, index = 0):
             res, index = __parse_boolean(string, index)
         elif string[index] == '[' or string[index] == '{':
             res, index = parse(string, index)
-        result, key = __append(result, res, type, key)
+        result, key = __append(result, res, type_, key)
 
         if string[index] == ']' or string[index] == '}':
-            index+=1
+            index += 1
             if index == len(string):
                 return result
-            return (result, index)
+            return result, index
         else:
             index += 1
 
-# TODO Interprete function
-#def interprete():
 
 def __skipp_white_spaces(string, index):
     while string[index] == ' ' or string[index] == '\t':
         index += 1
-
     return index
 
-def __append(result, res, type, key):
-    if type == 'dict':
+
+def __append(result, res, type_, key):
+    if type_ == 'dict':
         result[key] = res
         key += 1
     else:
         result.append(res)
     return result, key
+
 
 def __parse_string(string, index):
     if string[index] == '\'':
@@ -99,13 +108,14 @@ def __parse_string(string, index):
         index += 1
     index += 1
 
-    return (string[start:index-1], index)
+    return string[start:index-1], index
+
 
 def __parse_number(string, index):
     is_negative = False
     if string[index] == '-':
         is_negative = True
-        index+=1
+        index += 1
     start = index
 
     while str.isdigit(string[index]) or string[index] == '.':
@@ -117,18 +127,20 @@ def __parse_number(string, index):
         res = float(string[start:index])
     if is_negative:
         res = 0 - res
-    return (res, index)
+    return res, index
+
 
 def __parse_boolean(string, index):
     if string[index: index + 4] == 'true':
-        return (True, index + 4)
+        return True, index + 4
     elif string[index: index + 5] == 'false':
-        return (False, index + 5)
+        return False, index + 5
     else:
-        return (None, index )
+        return None, index
+
 
 def __parse_undefined(string, index):
     if string[index: index + 9] == 'undefined':
-        return (None, index + 9)
+        return None, index + 9
     else:
-        return (None, index)
+        return None, index
--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,18 +1,19 @@
-import jstp_python as api_jstp
 import unittest
+from src.jstp import jstp
+
 
 class TestCase(unittest.TestCase):
     def test1(self):
         example = ['Marcus Aurelius', 'AE127095', ['1990-02-15', 'Rome'],\
                    ['Ukraine', 'Kiev', '03056', 'Pobedy', '37', '158', '']]
         test = "['Marcus Aurelius','AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy','37','158','']]"
-        data = api_jstp.deserialize(test)
+        data = jstp.deserialize(test)
         assert data == example
 
     def test2(self):
         example = ['Marcus Aurelius', 'AE127095', ['1990-02-15', 'Rome'], ['Ukraine', 'Kiev', '03056', 'Pobedy', 37, 158]]
         test = "['Marcus Aurelius','AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy',37,158]]"
-        data = api_jstp.deserialize(test)
+        data = jstp.deserialize(test)
         assert data == example
 
     def test3(self):
@@ -20,14 +21,14 @@ class TestCase(unittest.TestCase):
                    ['Ukraine', 'Kiev', '03056', 'Pobedy', -37, None], False, True]
         test = "['Marcus Aurelius','AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy',-37,undefined]" \
                 ",false,true]"
-        data = api_jstp.deserialize(test)
+        data = jstp.deserialize(test)
         assert data == example
 
     def test4(self):
         example = {0: 'Marcus Aurelius', 1: 'Marcus Aurelius', 2: 'AE127095', 3: ['1990-02-15', 'Rome'],\
          4: ['Ukraine', 'Kiev', '03056', 'Pobedy', -37, 158]}
         test = "{'Marcus Aurelius', 'AE127095',['1990-02-15','Rome'],['Ukraine','Kiev','03056','Pobedy',-37,158]}"
-        data = api_jstp.parse(test)
+        data = jstp.parse(test)
         assert data == example
 
 if __name__ == '__main__':
